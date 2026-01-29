426a24e73d2fbf90e002e332fd2b5b829d9a2528
sizlik <zenjagems@gmail.com>
2026-01-29 22:45:39 +0200

    add batch

--- frontend/src/components/LiveClass.vue
+++ frontend/src/components/LiveClass.vue
@@ -11,104 +11,105 @@
 
 	<div class="flex items-center justify-between">
 		<div class="text-lg font-semibold text-ink-gray-9">
-			{{ __('Live Class') }}
+<!--			{{ __('Live Class') }}-->
+			Занятия
 		</div>
-		<Button v-if="canCreateClass()" @click="openLiveClassModal">
-			<template #prefix>
-				<Plus class="h-4 w-4" />
-			</template>
-			<span>
-				{{ __('Add') }}
-			</span>
-		</Button>
+<!--		<Button v-if="canCreateClass()" @click="openLiveClassModal">-->
+<!--			<template #prefix>-->
+<!--				<Plus class="h-4 w-4" />-->
+<!--			</template>-->
+<!--			<span>-->
+<!--				{{ __('Add') }}-->
+<!--			</span>-->
+<!--		</Button>-->
 	</div>
-	<div
-		v-if="liveClasses.data?.length"
-		class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-5"
-	>
-		<div
-			v-for="cls in liveClasses.data"
-			class="flex flex-col border rounded-md h-full text-ink-gray-7 hover:border-outline-gray-3 p-3"
-			:class="{
-				'cursor-pointer': hasPermission() && cls.attendees > 0,
-			}"
-			@click="
-				() => {
-					openAttendanceModal(cls)
-				}
-			"
-		>
-			<div class="font-semibold text-ink-gray-9 text-lg mb-1">
-				{{ cls.title }}
-			</div>
-			<div class="short-introduction">
-				{{ cls.description }}
-			</div>
-			<div class="mt-auto space-y-3">
-				<div class="flex items-center space-x-2">
-					<Calendar class="w-4 h-4 stroke-1.5" />
-					<span>
-						{{ dayjs(cls.date).format('DD MMMM YYYY') }}
-					</span>
-				</div>
-				<div class="flex items-center space-x-2">
-					<Clock class="w-4 h-4 stroke-1.5" />
-					<span>
-						{{ dayjs(getClassStart(cls)).format('hh:mm A') }} -
-						{{ dayjs(getClassEnd(cls)).format('hh:mm A') }}
-					</span>
-				</div>
-				<div
-					v-if="canAccessClass(cls)"
-					class="flex items-center space-x-2 text-ink-gray-9 mt-auto"
-				>
-					<a
-						v-if="user.data?.is_moderator || user.data?.is_evaluator"
-						:href="cls.start_url"
-						target="_blank"
-						class="cursor-pointer inline-flex items-center justify-center gap-2 transition-colors focus:outline-none text-ink-gray-8 bg-surface-gray-2 hover:bg-surface-gray-3 active:bg-surface-gray-4 focus-visible:ring focus-visible:ring-outline-gray-3 h-7 text-base px-2 rounded"
-						:class="cls.join_url ? 'w-full' : 'w-1/2'"
-					>
-						<Monitor class="h-4 w-4 stroke-1.5" />
-						{{ __('Start') }}
-					</a>
-					<a
-						:href="cls.join_url"
-						target="_blank"
-						class="w-full cursor-pointer inline-flex items-center justify-center gap-2 transition-colors focus:outline-none text-ink-gray-8 bg-surface-gray-2 hover:bg-surface-gray-3 active:bg-surface-gray-4 focus-visible:ring focus-visible:ring-outline-gray-3 h-7 text-base px-2 rounded"
-					>
-						<Video class="h-4 w-4 stroke-1.5" />
-						{{ __('Join') }}
-					</a>
-				</div>
-				<Tooltip
-					v-else-if="hasClassEnded(cls)"
-					:text="__('This class has ended')"
-					placement="right"
-				>
-					<div class="flex items-center space-x-2 text-ink-amber-3 w-fit">
-						<Info class="w-4 h-4 stroke-1.5" />
-						<span>
-							{{ __('Ended') }}
-						</span>
-					</div>
-				</Tooltip>
-			</div>
-		</div>
-	</div>
-	<div v-else class="text-sm italic text-ink-gray-5 mt-2">
-		{{ __('No live classes scheduled') }}
-	</div>
+<!--	<div-->
+<!--		v-if="liveClasses.data?.length"-->
+<!--		class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-5"-->
+<!--	>-->
+<!--		<div-->
+<!--			v-for="cls in liveClasses.data"-->
+<!--			class="flex flex-col border rounded-md h-full text-ink-gray-7 hover:border-outline-gray-3 p-3"-->
+<!--			:class="{-->
+<!--				'cursor-pointer': hasPermission() && cls.attendees > 0,-->
+<!--			}"-->
+<!--			@click="-->
+<!--				() => {-->
+<!--					openAttendanceModal(cls)-->
+<!--				}-->
+<!--			"-->
+<!--		>-->
+<!--			<div class="font-semibold text-ink-gray-9 text-lg mb-1">-->
+<!--				{{ cls.title }}-->
+<!--			</div>-->
+<!--			<div class="short-introduction">-->
+<!--				{{ cls.description }}-->
+<!--			</div>-->
+<!--			<div class="mt-auto space-y-3">-->
+<!--				<div class="flex items-center space-x-2">-->
+<!--					<Calendar class="w-4 h-4 stroke-1.5" />-->
+<!--					<span>-->
+<!--						{{ dayjs(cls.date).format('DD MMMM YYYY') }}-->
+<!--					</span>-->
+<!--				</div>-->
+<!--				<div class="flex items-center space-x-2">-->
+<!--					<Clock class="w-4 h-4 stroke-1.5" />-->
+<!--					<span>-->
+<!--						{{ dayjs(getClassStart(cls)).format('hh:mm A') }} - -->
+<!--						{{ dayjs(getClassEnd(cls)).format('hh:mm A') }}-->
+<!--					</span>-->
+<!--				</div>-->
+<!--				<div-->
+<!--					v-if="canAccessClass(cls)"-->
+<!--					class="flex items-center space-x-2 text-ink-gray-9 mt-auto"-->
+<!--				>-->
+<!--					<a-->
+<!--						v-if="user.data?.is_moderator || user.data?.is_evaluator"-->
+<!--						:href="cls.start_url"-->
+<!--						target="_blank"-->
+<!--						class="cursor-pointer inline-flex items-center justify-center gap-2 transition-colors focus:outline-none text-ink-gray-8 bg-surface-gray-2 hover:bg-surface-gray-3 active:bg-surface-gray-4 focus-visible:ring focus-visible:ring-outline-gray-3 h-7 text-base px-2 rounded"-->
+<!--						:class="cls.join_url ? 'w-full' : 'w-1/2'"-->
+<!--					>-->
+<!--						<Monitor class="h-4 w-4 stroke-1.5" />-->
+<!--						{{ __('Start') }}-->
+<!--					</a>-->
+<!--					<a-->
+<!--						:href="cls.join_url"-->
+<!--						target="_blank"-->
+<!--						class="w-full cursor-pointer inline-flex items-center justify-center gap-2 transition-colors focus:outline-none text-ink-gray-8 bg-surface-gray-2 hover:bg-surface-gray-3 active:bg-surface-gray-4 focus-visible:ring focus-visible:ring-outline-gray-3 h-7 text-base px-2 rounded"-->
+<!--					>-->
+<!--						<Video class="h-4 w-4 stroke-1.5" />-->
+<!--						{{ __('Join') }}-->
+<!--					</a>-->
+<!--				</div>-->
+<!--				<Tooltip-->
+<!--					v-else-if="hasClassEnded(cls)"-->
+<!--					:text="__('This class has ended')"-->
+<!--					placement="right"-->
+<!--				>-->
+<!--					<div class="flex items-center space-x-2 text-ink-amber-3 w-fit">-->
+<!--						<Info class="w-4 h-4 stroke-1.5" />-->
+<!--						<span>-->
+<!--							{{ __('Ended') }}-->
+<!--						</span>-->
+<!--					</div>-->
+<!--				</Tooltip>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--	</div>-->
+<!--	<div v-else class="text-sm italic text-ink-gray-5 mt-2">-->
+<!--		{{ __('No live classes scheduled') }}-->
+<!--	</div>-->
 	<CalendarSchedule class="w-full h-full" />
 
-	<LiveClassModal
-		:batch="props.batch"
-		:zoomAccount="props.zoomAccount"
-		v-model="showLiveClassModal"
-		v-model:reloadLiveClasses="liveClasses"
-	/>
+<!--	<LiveClassModal-->
+<!--		:batch="props.batch"-->
+<!--		:zoomAccount="props.zoomAccount"-->
+<!--		v-model="showLiveClassModal"-->
+<!--		v-model:reloadLiveClasses="liveClasses"-->
+<!--	/>-->
 
-	<LiveClassAttendance v-model="showAttendance" :live_class="attendanceFor" />
+<!--	<LiveClassAttendance v-model="showAttendance" :live_class="attendanceFor" />-->
 </template>
 <script setup>
 import { createListResource, Button, Tooltip } from 'frappe-ui'
